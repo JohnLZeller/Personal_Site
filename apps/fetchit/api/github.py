2a1ee40bbea6f3e899b5cce1faacdb7c54ccf066
John Zeller <johnlzeller@gmail.com>
2017-03-05 13:20:25 -0800

    sort, and truncate file types

--- apps/fetchit/api/github.py
+++ apps/fetchit/api/github.py
@@ -5,6 +5,7 @@ from config import get_base_config
 
 import json
 import logging
+import operator
 import re
 import requests
 
@@ -17,9 +18,12 @@ config = get_base_config()
 
 class GithubAPI(object):
     API_URL = 'https://api.github.com/users/JohnLZeller'
+    PROFILE_URL = 'https://github.com/JohnLZeller'
     EVENTS_URL = '%s/events' % API_URL
     CONTRIB_PATTERN = r'(\s+)(\d+)(\scontributions)'
     FILETYPE_PATTERN = r'.*(\.\w+)'
+    LOCATION_DEFAULT = 'New York, NY'
+    MAX_FILE_TYPES = 4
 
     # TODO: Check all responses for {u'message': u'Bad credentials'}
 
@@ -83,10 +87,10 @@ class GithubAPI(object):
         url = '%s?access_token=%s' % (self.API_URL, self.ACCESS_TOKEN)
         r = requests.get(url)
         profile = json.loads(r.content)
-        return profile.get('location', 'New York, NY')  # TODO: Default?
+        return profile.get('location', self.LOCATION_DEFAULT)
 
     def total_commits(self):
-        r = requests.get('https://github.com/JohnLZeller')
+        r = requests.get(self.PROFILE_URL)
         github_page = BeautifulSoup(r.content)
         contrib_graph = github_page.find_all(
             'div', {'class', 'js-contribution-graph'}
@@ -98,12 +102,24 @@ class GithubAPI(object):
         return match.group(2) if match else None
 
     def file_types(self, commit_data):
-        file_types = []
+        file_types = {}
 
         for f in commit_data.get('files', ''):
             match = re.match(self.FILETYPE_PATTERN, f.get('filename', ''))
-            f = match.group(1) if match else ''
-            if f not in file_types:
-                file_types.append(f)
+            f = match.group(1) if match else None
+            if file_types.get(f):
+                file_types[f] += 1
+            elif f:
+                file_types[f] = 1
 
-        return ', '.join(set(file_types))
+        sorted_types = sorted(
+            file_types.items(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )
+        trunc_toplist = [k for k, v in sorted_types[:self.MAX_FILE_TYPES]]
+        file_types_str = ', '.join(trunc_toplist)
+        if len(sorted_types) > self.MAX_FILE_TYPES:
+            file_types_str += ', ...'
+
+        return file_types_str
