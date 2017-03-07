62ed4ea7df55e64d01ac4a1a234db9759f3dd6f7
John Zeller <johnlzeller@gmail.com>
2017-03-06 17:42:51 -0800

    Add a references page

--- application.py
+++ application.py
@@ -73,6 +73,30 @@ def home():
         form = send_email(form)
         section = 'contact'
 
+    references = [{
+        'image': 'https://media.licdn.com/mpr/mpr/shrinknp_100_100/AAEAAQAAAAAAAAFBAAAAJGE1ZjU0MDg4LTY5OGQtNGFiOS05ZGYzLTcwZjc4NzFkMTEzMA.jpg',
+        'name': 'Ricky Thomas',
+        'title': 'Solutions Engineer at Datadog',
+        'text': (
+            "Working with John for almost two years was a great experience. As a Solutions Engineer I'd escalate certain cases to "
+            "him I wasn't able to resolve. He would always give clear explanations and provide resolutions quickly! He's a fun, "
+            "friendly, genuine individual who know's what he's talking about and can quickly resolve issues effectively. John and "
+            "I did a hack day project together where he had built an entirely brand new dynamic UI for our project overnight. We "
+            "even won a prize! John is a great guy who loves what he does and is great at it! I'd recommend him to any company."
+        )},
+        {
+        'image': 'https://media.licdn.com/mpr/mpr/shrinknp_100_100/p/8/000/1ed/0fc/325cbcf.jpg',
+        'name': 'David Merrick',
+        'title': 'Software Quality Engineer at Jive Software',
+        'text': (
+            "When I met John, he was enrolled in a full credit load in the engineering program at Oregon State University, leading "
+            "the Mars Rover team for the Robotics Club, and singlehandedly spearheading Advocates for Space Exploration, the "
+            "nonprofit he founded that later gained international media attention and drove over 31,000 petition signatures on "
+            "Change.org. He is one of the most ambitious people I know, yet is also one of the most trustworthy, creative, and "
+            "friendly. He is an excellent leader, and the enthusiasm he brings to everything he does is inspiring."
+        )},
+    ]
+
     return render_template(
         'index.html',
         section=section,
@@ -81,7 +105,7 @@ def home():
         commit=get_json(conn, 'github_data'),
         current_temp=get_json(conn, 'temp_f'),
         nutrition_info=get_json(conn, 'myfitnesspal_data'),
-        blog_posts=[]
+        references=references
     )
 
 if __name__ == '__main__':
